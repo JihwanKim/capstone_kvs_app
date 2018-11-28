5116534a01022039a53b359bfb06c28ad8538fe0
taeyoungOh <ohtaeyoung12@naver.com>
2018-11-29 02:06:00 +0900

    기존 기록액티비티에서 메뉴바 없애고 모션이벤트로 그래프 두개를 번갈아가면서 확인할 수 있도록함.

--- app/src/main/java/com/example/user/kvs/FourthActivity.java
+++ app/src/main/java/com/example/user/kvs/FourthActivity.java
@@ -7,17 +7,30 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class FourthActivity extends AppCompatActivity{
+    int tog = 0;
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(new MyGraphicView(this));
+
     }
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_UP:
+                tog ^= 1;
+                if(tog == 0){
+                    setContentView(new MyGraphicView(this));
+                }
+                else{
+                    setContentView(new GraphicView(this));
+                }
+        }
+        return true;
+    }
+
     private static class MyGraphicView extends View {   //운전자의 상태기록 그래픽뷰
         public MyGraphicView(Context context) {
             super(context);
@@ -29,87 +42,72 @@ public class FourthActivity extends AppCompatActivity{
             paint.setAntiAlias(true);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(20);                              //글짜 표기
-            canvas.drawText("운전자의 상태 기록", 10, 30, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(50);                              //글짜 표기
+            canvas.drawText("운전자의 상태 기록", 10, 50, paint);	//표시명, 시작점의 xy좌표
+
+            //갤럭시 A5 -> 720 * 1280
 
             paint.setColor(Color.GREEN);                //paint객체의 색상 초록색 지정
-            paint.setTextSize(10);                              //글짜 표기
-            canvas.drawText("100%", 455, 40, paint);
-            canvas.drawLine(0, 50, 480, 50, paint);	//100%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
-            canvas.drawText("75%", 460, 190, paint);
-            canvas.drawLine(0, 200, 480, 200, paint);	//75%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
-            canvas.drawText("50%", 460, 340, paint);
-            canvas.drawLine(0, 350, 480, 350, paint);	//50%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
-            canvas.drawText("25%", 460, 490, paint);
-            canvas.drawLine(0, 500, 480, 500, paint);	//25%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            paint.setTextSize(30);                              //글짜 표기
+            canvas.drawText("100%", 640, 110, paint);
+            canvas.drawLine(0, 120, 720, 120, paint);	//100%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
+            canvas.drawText("80%", 660, 110+200, paint);
+            canvas.drawLine(0, 120+200, 720, 120+200, paint);	//80%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
+            canvas.drawText("60%", 660, 110+400, paint);
+            canvas.drawLine(0, 120+400, 720, 120+400, paint);	//60%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            canvas.drawText("40%", 660, 110+600, paint);
+            canvas.drawLine(0, 120+600, 720, 120+600, paint);	//40%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            canvas.drawText("20%", 660, 110+800, paint);
+            canvas.drawLine(0, 120+800, 720, 120+800, paint);	//20%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
 
             paint.setColor(Color.BLACK);
-            canvas.drawLine(0, 650, 480, 650, paint);	//0%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            canvas.drawText("0%", 680, 110+1000, paint);
+            canvas.drawLine(0, 120+1000, 720, 120+1000, paint);	//0%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
 
             paint.setStyle(Paint.Style.FILL);		//내부를 색상 채워라(FILL)
 
-            //&&&&&100% 75% 50% 25% 0%-> top = 50, 200, 350, 500, 650으로 설정하면됨.&&&&&&
+            //&&&&&100% 80% 60% 40% 20% 0%-> top = 120, 320, 520, 720, 920 1120으로 설정하면됨.&&&&&&
 
             paint.setColor(Color.RED);			//paint객체의 색상 다시 빨간색으로 지정
-            Rect rect1 = new Rect(15, 50, 15+30, 250+400); //첫번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect1 = new Rect(50, 120, 50+50, 120+1000); //첫번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect1, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("man", 17, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("man", 50, 1160, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect2 = new Rect(75, 350, 75+30, 250+400); //두번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect2 = new Rect(50+110, 120+200, 50+110+50, 120+1000); //두번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect2, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("smiling", 67, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("smiling", 50+110, 1160, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect3 = new Rect(135, 200, 135+30, 250+400); //세번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect3 = new Rect(50+220, 120+400, 50+220+50, 120+1000); //세번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect3, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("eye", 140, 680, paint);	//표시명, 시작점의 xy좌표
-            canvas.drawText("open", 135, 700, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("eye", 50+220, 1160, paint);	//표시명, 시작점의 xy좌표
+            canvas.drawText("open", 50+220, 1180, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect4 = new Rect(195, 50, 195+30, 250+400); //네번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect4 = new Rect(50+330, 120+600, 50+330+50, 120+1000); //네번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect4, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("age", 200, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("age", 50+330, 1160, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect5 = new Rect(255, 100, 255+30, 250+400); //다섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect5 = new Rect(50+440, 120+800, 50+440+50, 120+1000); //다섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect5, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("xxxx", 255, 680, paint);	//표시명, 시작점의 xy좌표
-
-            paint.setColor(Color.RED);
-            Rect rect6 = new Rect(315, 225, 315+30, 250+400); //여섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
-            canvas.drawRect(rect6, paint);
-
-            paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("xxxx", 315, 680, paint);	//표시명, 시작점의 xy좌표
-
-            paint.setColor(Color.RED);
-            Rect rect7 = new Rect(375, 275, 375+30, 250+400); //여섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
-            canvas.drawRect(rect7, paint);
-
-            paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("xxxx", 375, 680, paint);	//표시명, 시작점의 xy좌표
-
-
-
-
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("xxxx", 50+440, 1160, paint);	//표시명, 시작점의 xy좌표
         }
     }
 
@@ -124,109 +122,73 @@ public class FourthActivity extends AppCompatActivity{
             paint.setAntiAlias(true);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(20);                              //글짜 표기
-            canvas.drawText("표정 분석 결과 기록", 10, 30, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(50);                              //글짜 표기
+            canvas.drawText("운전자의 상태 기록", 10, 50, paint);	//표시명, 시작점의 xy좌표
+
+            //갤럭시 A5 -> 720 * 1280
 
             paint.setColor(Color.GREEN);                //paint객체의 색상 초록색 지정
-            paint.setTextSize(10);                              //글짜 표기
-            canvas.drawText("100%", 455, 40, paint);
-            canvas.drawLine(0, 50, 480, 50, paint);	//100%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
-            canvas.drawText("75%", 460, 190, paint);
-            canvas.drawLine(0, 200, 480, 200, paint);	//75%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
-            canvas.drawText("50%", 460, 340, paint);
-            canvas.drawLine(0, 350, 480, 350, paint);	//50%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
-            canvas.drawText("25%", 460, 490, paint);
-            canvas.drawLine(0, 500, 480, 500, paint);	//25%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            paint.setTextSize(30);                              //글짜 표기
+            canvas.drawText("100%", 640, 110, paint);
+            canvas.drawLine(0, 120, 720, 120, paint);	//100%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
+            canvas.drawText("80%", 660, 110+200, paint);
+            canvas.drawLine(0, 120+200, 720, 120+200, paint);	//80%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)             //글짜 표기
+            canvas.drawText("60%", 660, 110+400, paint);
+            canvas.drawLine(0, 120+400, 720, 120+400, paint);	//60%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            canvas.drawText("40%", 660, 110+600, paint);
+            canvas.drawLine(0, 120+600, 720, 120+600, paint);	//40%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            canvas.drawText("20%", 660, 110+800, paint);
+            canvas.drawLine(0, 120+800, 720, 120+800, paint);	//20%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
 
             paint.setColor(Color.BLACK);
-            canvas.drawLine(0, 650, 480, 650, paint);	//0%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
+            canvas.drawText("0%", 680, 110+1000, paint);
+            canvas.drawLine(0, 120+1000, 720, 120+1000, paint);	//0%표시선 그리기(시작점의 xy좌표, 끝점의 xy좌표)
 
             paint.setStyle(Paint.Style.FILL);		//내부를 색상 채워라(FILL)
 
+            //&&&&&100% 80% 60% 40% 20% 0%-> top = 120, 320, 520, 720, 920 1120으로 설정하면됨.&&&&&&
+
             paint.setColor(Color.RED);			//paint객체의 색상 다시 빨간색으로 지정
-            Rect rect1 = new Rect(15, 400, 15+30, 250+400); //첫번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect1 = new Rect(50, 120+800, 50+50, 120+1000); //첫번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect1, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("Happy", 10, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("man", 50, 1160, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect2 = new Rect(75, 100, 75+30, 250+400); //두번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect2 = new Rect(50+110, 120+600, 50+110+50, 120+1000); //두번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect2, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("angry", 75, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("smiling", 50+110, 1160, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect3 = new Rect(135, 50, 135+30, 250+400); //두번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect3 = new Rect(50+220, 120+400, 50+220+50, 120+1000); //세번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect3, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("urgent", 130, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("eye", 50+220, 1160, paint);	//표시명, 시작점의 xy좌표
+            canvas.drawText("open", 50+220, 1180, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect4 = new Rect(195, 225, 195+30, 250+400); //두번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect4 = new Rect(50+330, 120+200, 50+330+50, 120+1000); //네번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect4, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("tired", 195, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("age", 50+330, 1160, paint);	//표시명, 시작점의 xy좌표
 
             paint.setColor(Color.RED);
-            Rect rect5 = new Rect(255, 500, 255+30, 250+400); //다섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
+            Rect rect5 = new Rect(50+440, 120, 50+440+50, 120+1000); //다섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
             canvas.drawRect(rect5, paint);
 
             paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("xxxx", 255, 680, paint);	//표시명, 시작점의 xy좌표
-
-            paint.setColor(Color.RED);
-            Rect rect6 = new Rect(315, 75, 315+30, 250+400); //여섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
-            canvas.drawRect(rect6, paint);
-
-            paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("xxxx", 315, 680, paint);	//표시명, 시작점의 xy좌표
-
-            paint.setColor(Color.RED);
-            Rect rect7 = new Rect(375, 50, 375+30, 250+400); //여섯번째 바 (시작점의 xy좌표, 가로간격, 세로간격)
-            canvas.drawRect(rect7, paint);
-
-            paint.setColor(Color.BLACK);
-            paint.setTextSize(15);                              //글짜 표기
-            canvas.drawText("xxxx", 375, 680, paint);	//표시명, 시작점의 xy좌표
+            paint.setTextSize(25);                              //글짜 표기
+            canvas.drawText("xxxx", 50+440, 1160, paint);	//표시명, 시작점의 xy좌표
         }
     }
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        menu.add(0,1,0, "운전자의 상태 기록");
-        menu.add(0,2,0, "표정 분석 결과 기록");
-        menu.add(0,3,0, "돌아 가기");
-        //SubMenu sMenu = menu.addSubMenu("색상 변경 >>");  //서브메뉴 필요하면 사용할 수 있음.
-        //sMenu.add(0,4,0, "빨강");
-        return true;
-    }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case 1:
-                setContentView(new MyGraphicView(this));
-                return true;
-
-            case 2:
-                setContentView(new GraphicView(this));
-                return true;
-            case 3:
-                finish();
-                Toast.makeText(FourthActivity.this, "돌아가기 버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show();
-                return true;
-            //case 4:
-
-            //return true;
-        }
-        return  super.onOptionsItemSelected(item);
-    }
 }
